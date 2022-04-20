1435465e3dc579be709e40e20f2eb0dc21186c75
AugustinSorel <sorelaugustin@gmail.com>
2022-04-20 18:30:34 +0200

    init project

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/health-check", (req, res) => {
+  res.sendStatus(200);
+});
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
