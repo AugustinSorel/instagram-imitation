45cb210c39958ebd3a99aad5e2a04ab122e5f3f3
AugustinSorel <sorelaugustin@gmail.com>
2022-04-27 14:25:32 +0200

    heroku test

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,15 @@ import path from "path";
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.get("/health-check", (req, res) => {
+  res.sendStatus(200);
+});
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("/*", function (req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 }
-
-app.get("/health-check", (req, res) => {
-  res.sendStatus(200);
-});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
