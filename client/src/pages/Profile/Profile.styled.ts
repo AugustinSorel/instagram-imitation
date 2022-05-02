d8d01000a5eba589e46ccb7f88a0c37421150242
AugustinSorel <sorelaugustin@gmail.com>
2022-05-02 19:42:19 +0100

    basic grid added

--- client/src/pages/Profile/Profile.styled.ts
+++ client/src/pages/Profile/Profile.styled.ts
@@ -4,7 +4,35 @@ export const ProfileContainer = styled.div`
   overflow: auto;
 `;
 
+export const ProfileHeader = styled.header`
+  position: sticky;
+  top: 0;
+  padding: 1rem;
+  margin-bottom: 1rem;
+  background-color: var(--background);
+
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const ProfileHeaderUsername = styled.h1`
+  font-size: var(--font-size-large);
+  font-weight: var(--font-weight-regular);
+  text-transform: capitalize;
+
+  flex: 1;
+`;
+
+export const ProfileHeaderAvatar = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+
 export const Grid = styled.div`
+  padding: 0 1rem;
   display: grid;
   gap: 1rem;
 
--- client/src/pages/Profile/index.tsx
+++ client/src/pages/Profile/index.tsx
@@ -1,23 +1,33 @@
+import { useQueryClient } from "react-query";
+import { AuthenticationFormState } from "../../shared/components/UIElements/authenticationForm/AuthenticationReducer";
 import MaxWidthWrapper from "../../shared/components/wrappers/MaxWidthWrapper";
-import { Grid, GridItem, ProfileContainer } from "./Profile.styled";
+import {
+  Grid,
+  GridItem,
+  ProfileContainer,
+  ProfileHeader,
+  ProfileHeaderAvatar,
+  ProfileHeaderUsername,
+} from "./Profile.styled";
 
 const Profile = () => {
+  const queryClient = useQueryClient();
+  const user = queryClient.getQueryData("user") as AuthenticationFormState;
+
   return (
     <ProfileContainer>
       <MaxWidthWrapper>
-        <h1>Profile</h1>
+        <ProfileHeader>
+          <ProfileHeaderUsername>{user.username}</ProfileHeaderUsername>
+          <ProfileHeaderAvatar
+            src="https://sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png"
+            alt="userAvatar"
+          />
+        </ProfileHeader>
         <Grid>
-          <GridItem />
-          <GridItem />
-          <GridItem />
-          <GridItem />
-          <GridItem />
-          <GridItem />
-          <GridItem />
-          <GridItem />
-          <GridItem />
-          <GridItem />
-          <GridItem />
+          {[...Array(10)].map((_, i) => (
+            <GridItem key={i} />
+          ))}
         </Grid>
       </MaxWidthWrapper>
     </ProfileContainer>
