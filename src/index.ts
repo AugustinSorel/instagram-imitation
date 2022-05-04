e2fb9b1be42edb527da6516bb4003548f07e068d
AugustinSorel <sorelaugustin@gmail.com>
2022-05-04 16:50:07 +0100

    heroku test

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ import deserializeUser from "./middlewares/deserializeUser";
 import router from "./routes";
 import connectToMongoDB from "./utils/connectToMongoDB.util";
 import deployToProduction from "./utils/deployToProduction.util";
-import path from "path";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,14 +20,7 @@ app.use(deserializeUser);
 app.use("/api", router);
 app.use(apiError);
 
-// deployToProduction(app);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
-}
+deployToProduction(app);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
--- src/utils/deployToProduction.util.ts
+++ src/utils/deployToProduction.util.ts
@@ -4,9 +4,9 @@ import path from "path";
 
 const deployToProduction = (app: Express) => {
   if (process.env.NODE_ENV === "production") {
-    app.use(express.static("../client/build"));
+    app.use(express.static("client/build"));
     app.get("*", (req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, "../client/build/index.html"));
+      res.sendFile(path.join(__dirname, "../../client/build/index.html"));
     });
   }
 };
