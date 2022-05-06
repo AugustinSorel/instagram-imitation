edb7256c44532778611302b9b6a87763db62ba76
AugustinSorel <sorelaugustin@gmail.com>
2022-05-06 12:01:36 +0100

    retrieve user posts

--- src/controllers/posts.controllers.ts
+++ src/controllers/posts.controllers.ts
@@ -5,7 +5,7 @@ import PostModel from "../models/Post.model";
 import UserModel from "../models/User.model";
 import { AddNewPostSchema } from "../schemas/posts.schema";
 import { uploadPost } from "../services/cloudinary.service";
-import { userAddNewPost } from "../services/user.service";
+import { findById, userAddNewPost } from "../services/user.service";
 
 export const addNewPost = async (
   req: Request<{}, {}, AddNewPostSchema>,
@@ -25,6 +25,29 @@ export const addNewPost = async (
     res.sendStatus(200);
   } catch (error) {
     console.log("ERROR in addNewPost", error);
+    res.sendStatus(500);
+  }
+};
+
+export const getUserPosts = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await findById(res.locals.userId);
+
+    if (!user) {
+      return next(AuthError.invalidIdError());
+    }
+
+    const userPosts = user.posts;
+
+    const posts = await PostModel.find({ _id: { $in: userPosts } });
+
+    res.json(posts);
+  } catch (error) {
+    console.log("ERROR in getUserPosts", error);
     res.sendStatus(500);
   }
 };
--- src/routes/posts.routes.ts
+++ src/routes/posts.routes.ts
@@ -1,11 +1,13 @@
 import express from "express";
-import { addNewPost } from "../controllers/posts.controllers";
+import { addNewPost, getUserPosts } from "../controllers/posts.controllers";
 import requireUser from "../middlewares/requireUser";
 import validateRessource from "../middlewares/validateRessource.middleware";
 import { addNewPostSchema } from "../schemas/posts.schema";
 import upload from "../utils/multer.util";
 
 const postsRouter = express.Router();
+
+postsRouter.get("/", requireUser, getUserPosts);
 
 postsRouter.post(
   "/add-new-post",
