96faecc446de663e3b69da75d0177a55718719fd
AugustinSorel <sorelaugustin@gmail.com>
2023-04-02 11:09:13 +0100

    p tag removed from button

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -29,17 +29,15 @@ const NewPostButton = () => {
 
 const QuickSearchButton = () => {
   return (
-    <button className="flex w-80 items-center gap-2 rounded-md border border-black/10 p-2 backdrop-blur-sm duration-300 hover:bg-white/40">
+    <button className="flex w-80 items-center gap-2 rounded-md border border-black/10 p-2 text-sm capitalize text-slate-600 backdrop-blur-sm duration-300 hover:bg-white/40">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
-        className="aspect-square w-4 fill-slate-600"
+        className="aspect-square w-4 fill-current"
       >
         <path d="M23.809 21.646l-6.205-6.205c1.167-1.605 1.857-3.579 1.857-5.711 0-5.365-4.365-9.73-9.731-9.73-5.365 0-9.73 4.365-9.73 9.73 0 5.366 4.365 9.73 9.73 9.73 2.034 0 3.923-.627 5.487-1.698l6.238 6.238 2.354-2.354zm-20.955-11.916c0-3.792 3.085-6.877 6.877-6.877s6.877 3.085 6.877 6.877-3.085 6.877-6.877 6.877c-3.793 0-6.877-3.085-6.877-6.877z" />
       </svg>
-
-      <p className="text-sm capitalize text-slate-600">quick search...</p>
-
+      quick search...
       <kbd className="ml-auto text-sm capitalize text-slate-400">
         <abbr className="no-underline" title="Control">
           ctrl
