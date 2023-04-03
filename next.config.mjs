5de8d6828dd572b6e99cf8e94c44c4b3d5ae4a76
AugustinSorel <sorelaugustin@gmail.com>
2023-04-03 17:50:35 +0100

    images domain added

--- next.config.mjs
+++ next.config.mjs
@@ -18,5 +18,8 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  images: {
+    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
+  },
 };
 export default config;
