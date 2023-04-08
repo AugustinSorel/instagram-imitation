fb1caf26565451a36b275ad31a0902aaab9a2dad
AugustinSorel <sorelaugustin@gmail.com>
2023-04-08 21:53:05 +0200

    post router added

---
+++ src/server/api/routers/post.ts
@@ -0,0 +1,30 @@
+import { newPostSchema } from "~/components/Layout";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { z } from "zod";
+
+export const postRouter = createTRPCRouter({
+  newPost: protectedProcedure
+    .input(
+      z.object({
+        location: z
+          .string({ required_error: "location is required" })
+          .min(1, "location must be at least one character")
+          .max(255, "location must be at least 255 characters"),
+        description: z
+          .string({ required_error: "description is required" })
+          .min(1, "location must be at least one character")
+          .max(2047, "location must be at least 255 characters"),
+        images: z
+          .string()
+          .url()
+          .array()
+          .min(1, "minimum of one image is allowed")
+          .max(5, "maximum of five images is allowed"),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.prisma.post.create({
+        data: { ...input, userId: ctx.session.user.id },
+      });
+    }),
+});
