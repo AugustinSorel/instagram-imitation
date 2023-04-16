8a21782c6ab44a465a0f638986140dc595ba34b0
AugustinSorel <sorelaugustin@gmail.com>
2023-04-16 11:06:42 +0200

    comments and likes position changed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,9 @@ import { appRouter } from "~/server/api/root";
 import { prisma } from "~/server/db";
 import { api, type RouterOutputs } from "~/utils/api";
 
+//TODO: fix position of comment and like
+//TODO: increase image quality
+
 const SkeletonPost = () => {
   return (
     <div className="relative flex h-post w-post flex-col justify-between overflow-hidden rounded-3xl border border-black/20 bg-white/20 p-2 shadow-2xl backdrop-blur-md after:absolute after:bottom-0 after:left-0 after:top-0 after:w-32 after:rotate-[20deg] after:scale-150 after:animate-post-skeleton after:bg-black/10 after:blur-xl">
@@ -80,13 +83,29 @@ const Post = ({ post }: PostProps) => {
           </p>
         </div>
 
-        <button
-          title="bookmark this post"
-          name="bookmark this post"
-          className="aspect-square rounded-full border border-black/20 bg-white/50 fill-slate-600 p-2 opacity-0 backdrop-blur-md duration-300 hover:bg-white/80 hover:fill-slate-900 focus-visible:bg-white/80 focus-visible:fill-slate-900 focus-visible:opacity-100 group-hover:opacity-100"
-        >
-          <SvgIcon svgName="bookmark" />
-        </button>
+        <div className="space-x-2 fill-slate-600">
+          <button
+            title="bookmark this post"
+            name="bookmark this post"
+            className="aspect-square rounded-full border border-black/20 bg-white/50 fill-slate-600 p-2 opacity-0 backdrop-blur-md duration-300 hover:bg-white/80 hover:fill-slate-900 focus-visible:bg-white/80 focus-visible:fill-slate-900 focus-visible:opacity-100 group-hover:opacity-100"
+          >
+            <SvgIcon svgName="bookmark" />
+          </button>
+          <button
+            title="view comments"
+            name="view comments"
+            className="aspect-square rounded-full border border-black/20 bg-white/50 p-2 opacity-0 backdrop-blur-md duration-300 hover:bg-white/80 hover:fill-slate-900 focus-visible:bg-white/80 focus-visible:fill-slate-900 focus-visible:opacity-100 group-hover:opacity-100"
+          >
+            <SvgIcon svgName="speech" />
+          </button>
+          <button
+            title="like"
+            name="like"
+            className="aspect-square rounded-full border border-black/20 bg-white/50 p-2 opacity-0 backdrop-blur-md duration-300 hover:bg-white/80 hover:fill-slate-900 focus-visible:bg-white/80 focus-visible:fill-slate-900 focus-visible:opacity-100 group-hover:opacity-100"
+          >
+            <SvgIcon svgName="heart" />
+          </button>
+        </div>
       </header>
 
       {post.images.length > 0 && (
@@ -110,7 +129,7 @@ const Post = ({ post }: PostProps) => {
         </nav>
       )}
 
-      <footer className="grid grid-cols-[1fr_auto_1fr] items-center fill-slate-600">
+      <footer className="mx-auto items-center ">
         {post.images.length > 1 && (
           <nav className="col-start-2 flex items-center gap-3">
             {[...Array<unknown>(post.images.length)].map((_, i) => (
@@ -125,23 +144,6 @@ const Post = ({ post }: PostProps) => {
             ))}
           </nav>
         )}
-
-        <div className="col-start-3 ml-auto space-x-3 ">
-          <button
-            title="view comments"
-            name="view comments"
-            className="aspect-square rounded-full border border-black/20 bg-white/50 p-2 opacity-0 backdrop-blur-md duration-300 hover:bg-white/80 hover:fill-slate-900 focus-visible:bg-white/80 focus-visible:fill-slate-900 focus-visible:opacity-100 group-hover:opacity-100"
-          >
-            <SvgIcon svgName="speech" />
-          </button>
-          <button
-            title="like"
-            name="like"
-            className="aspect-square rounded-full border border-black/20 bg-white/50 p-2 opacity-0 backdrop-blur-md duration-300 hover:bg-white/80 hover:fill-slate-900 focus-visible:bg-white/80 focus-visible:fill-slate-900 focus-visible:opacity-100 group-hover:opacity-100"
-          >
-            <SvgIcon svgName="heart" />
-          </button>
-        </div>
       </footer>
 
       <ul
