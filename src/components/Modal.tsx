aace681dc128a391aeee5974b3ecd6daf6f8e4fd
AugustinSorel <sorelaugustin@gmail.com>
2023-04-17 21:56:55 +0200

    modal props changed

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,24 +1,22 @@
-import { type ComponentProps } from "react";
+import type { PropsWithChildren, ComponentProps } from "react";
 import Backdrop from "./Backdrop";
 import { SvgIcon } from "./SvgIcon";
 
-const Modal = (props: ComponentProps<typeof Backdrop>) => {
-  const { children, clickHandler, animationEndHandler, isExpanded } = props;
+type Props = {
+  backdropProps: ComponentProps<typeof Backdrop>;
+} & PropsWithChildren;
 
+const Modal = ({ backdropProps, children }: Props) => {
   return (
-    <Backdrop
-      isExpanded={isExpanded}
-      animationEndHandler={animationEndHandler}
-      clickHandler={clickHandler}
-    >
+    <Backdrop {...backdropProps}>
       <div
-        className="relative h-full w-full overflow-auto bg-white/70 p-5 dark:bg-black/70 dark:fill-slate-300 dark:text-slate-300 lg:h-auto lg:max-h-[90%] lg:w-auto lg:max-w-[75%] lg:rounded-md "
+        className="relative m-auto h-full w-full overflow-auto bg-white/70 p-5 dark:bg-black/70 dark:fill-slate-300 dark:text-slate-300 lg:h-auto lg:max-h-[90%] lg:w-auto lg:max-w-[75%] lg:rounded-md "
         onClick={(e) => e.stopPropagation()}
       >
         <button
           title="Close Modal"
-          className="dark:hover:bg-white/10lg:hidden absolute right-3 top-3 flex aspect-square h-8 items-center justify-center rounded-md border border-black/10 bg-black/5 fill-slate-600 p-1 duration-300 hover:bg-black/10 dark:border-white/10 dark:bg-white/5 dark:fill-current dark:hover:bg-white/10"
-          onClick={clickHandler}
+          className="absolute right-3 top-3 flex aspect-square h-8 items-center justify-center rounded-md border border-black/10 bg-black/5 fill-slate-600 p-1 duration-300 hover:bg-black/10 dark:border-white/10 dark:bg-white/5 dark:fill-current dark:hover:bg-white/10 lg:hidden"
+          onClick={backdropProps.clickHandler}
         >
           <SvgIcon svgName="close" />
         </button>
