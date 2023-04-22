a29504c6586def3c56ae5110fe666d71eac60066
AugustinSorel <sorelaugustin@gmail.com>
2023-04-22 15:31:11 +0200

    user router merged

---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,17 @@
+import { createTRPCRouter, publicProcedure } from "../trpc";
+import { z } from "zod";
+
+export const UserRouter = createTRPCRouter({
+  byId: publicProcedure
+    .input(z.object({ id: z.string().cuid() }))
+    .query(async ({ input, ctx }) => {
+      return ctx.prisma.user.findUnique({
+        where: {
+          id: input.id,
+        },
+        include: {
+          posts: true,
+        },
+      });
+    }),
+});
