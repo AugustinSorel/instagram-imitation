3617b003c6d26c86d60c55ad918022b47ea8242f
AugustinSorel <sorelaugustin@gmail.com>
2023-04-23 22:28:02 +0200

    follow and unfollow routers added

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,12 +1,40 @@
-import { createTRPCRouter, publicProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 import { z } from "zod";
 
 export const UserRouter = createTRPCRouter({
   byId: publicProcedure
     .input(z.object({ id: z.string().cuid() }))
     .query(async ({ input, ctx }) => {
-      return ctx.prisma.user.findUnique({
+      return await ctx.prisma.user.findUnique({
         where: { id: input.id },
+        include: {
+          followedBy: true,
+          following: true,
+        },
+      });
+    }),
+
+  follow: protectedProcedure
+    .input(z.object({ id: z.string().cuid() }))
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.prisma.follows.create({
+        data: {
+          followerId: ctx.session.user.id,
+          followingId: input.id,
+        },
+      });
+    }),
+
+  unfollow: protectedProcedure
+    .input(z.object({ id: z.string().cuid() }))
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.prisma.follows.delete({
+        where: {
+          followerId_followingId: {
+            followerId: ctx.session.user.id,
+            followingId: input.id,
+          },
+        },
       });
     }),
 });
